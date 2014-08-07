7435bd2bbbe128dde853b2582984ee34cc120793
AkriosChan <lord-knight@163.com>
2014-08-07 11:34:47 +0800

    字符串拼接测试

---
+++ tests/stringCombine.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "fmt"
+	"strconv"
+)
+
+func main() {
+	//fmt.Println("aaaa")
+
+	var i int = 12222
+	var stra string = "a"
+	var strb string = "b"
+	var strc string = "c"
+	//fmt.Println(stra + strb + strc + i)
+	//fmt.Println(stra+strb+strc, strconv.Itoa(i))
+	println(stra+strb+strc, strconv.Itoa(i))
+}
